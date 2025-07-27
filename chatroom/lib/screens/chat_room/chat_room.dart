1d7705434d0841a4ea34879eb62a4b7dbe033cbf
nicole1drinkwater <nicnacsdrinkwater@gmail.com>
2025-07-27 16:54:36 +0200

    Added currentUser to user_store and alertDialog functionality in the signUp screen

--- chatroom/lib/screens/chat_room/chat_room.dart
+++ chatroom/lib/screens/chat_room/chat_room.dart
@@ -1,6 +1,10 @@
 import 'package:chatroom/screens/sign_up/sign_up.dart';
+import 'package:chatroom/services/message_store.dart';
+import 'package:chatroom/services/user_store.dart';
 import 'package:chatroom/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../models/message.dart';
 import '../../shared/styled_button.dart';
 import '../../shared/styled_text.dart';
 
@@ -12,6 +16,57 @@ class ChatRoomScreen extends StatefulWidget {
 }
 
 class _ChatRoomScreenState extends State<ChatRoomScreen> {
+  final _messageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void handleSubmit() {
+    if (_messageController.text.trim().isEmpty){
+      
+      showDialog(context: context, builder: (context) {
+        return AlertDialog(
+          title: const StyledHeading('Missing Text'),
+          content: const StyledText('Please enter your text.'),
+          actions: [
+            StyledButton(
+              onPressed: () {
+                Navigator.pop(context);
+              }, 
+              child: const StyledHeading('Close'),
+            ),
+          ],
+          actionsAlignment: MainAxisAlignment.center,
+        );
+      });
+
+      return;
+    
+    }
+    final userStore = Provider.of<UserStore>(context, listen: false);
+
+    final currentUser = userStore.currentUser;
+
+    final String senderId = currentUser!.id;
+
+     Provider.of<MessageStore>(context, listen: false) 
+    .addMessage(Message(
+      messageContent: _messageController.text.trim(),
+      timeSent: DateTime.now(),
+      senderID: senderId,
+      messageID: '',
+    ));
+
+      Navigator.push(context, MaterialPageRoute(
+      builder: (context) => const ChatRoomScreen(),
+    ));
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,6 +109,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
 
                 IconButton(
                   onPressed: () {
+                    handleSubmit();
                   },
                   icon: Icon(Icons.send, color: AppColors.primaryColor,),
                 ),
