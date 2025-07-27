cb5fa198b0ce4b74f8193b3066839b5fb70cb0eb
nicole1drinkwater <nicnacsdrinkwater@gmail.com>
2025-07-27 14:58:43 +0200

    Added feature to add a user to the database by entering their name in the sign up screen

--- chatroom/lib/models/user.dart
+++ chatroom/lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
   //Maybe profile pic later
   User({
@@ -6,4 +8,26 @@ class User {
 
   final String name;
   final String id;
+
+  // user to firestore (map)
+  Map<String, dynamic> toFireStore() {
+    return {
+      "name": name,
+    };
+  }
+
+  // user from firestore
+  factory User.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data()!;
+
+    User user = User(
+      name: data['name'],
+      id: snapshot.id,
+    );
+
+    return user;
+  } 
 }
--- chatroom/lib/services/firestore_service.dart
+++ chatroom/lib/services/firestore_service.dart
@@ -1,6 +1,16 @@
+import 'package:chatroom/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+//does automatic conversions
 class FirestoreService {
   static final ref = FirebaseFirestore.instance
-  .collection("users");
-}
+  .collection("users")
+  .withConverter(
+    fromFirestore: User.fromFirestore,
+    toFirestore: (User u, _) =>  u.toFireStore()
+  );
+
+  static Future<void> addUser(User user) async {
+    await ref.doc(user.id).set(user);
+  }
+ }
