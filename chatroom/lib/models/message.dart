49bb14745f3ac3cf23767385bad785deaa684e37
nicole1drinkwater <nicnacsdrinkwater@gmail.com>
2025-07-27 16:27:04 +0200

    Added message_store and its related code to firestore_service

--- chatroom/lib/models/message.dart
+++ chatroom/lib/models/message.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Message {
   Message({
     required this.messageContent, required this.messageID, required this.senderID, required this.timeSent,
@@ -7,4 +9,32 @@ class Message {
   final String messageID;
   final String senderID;
   final DateTime timeSent;
+
+  // user to firestore (map)
+  Map<String, dynamic> toFireStore() {
+    return {
+      "messageContent": messageContent,
+      "senderID": senderID,
+      "timeSent": FieldValue.serverTimestamp(),
+    };
+  }
+
+  // user from firestore
+  factory Message.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final data = snapshot.data()!;
+
+    final timestamp = data['timeSent'] as Timestamp;
+
+    Message user = Message(
+      messageContent: data['messageContent'],
+      senderID: data['senderID'],
+      messageID: snapshot.id,
+      timeSent: timestamp.toDate(),
+    );
+
+    return user;
+  } 
 }
--- chatroom/lib/services/firestore_service.dart
+++ chatroom/lib/services/firestore_service.dart
@@ -1,20 +1,36 @@
 import 'package:chatroom/models/user.dart';
+import 'package:chatroom/models/message.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 //does automatic conversions
 class FirestoreService {
-  static final ref = FirebaseFirestore.instance
+  static final userRef = FirebaseFirestore.instance
   .collection("users")
   .withConverter(
     fromFirestore: User.fromFirestore,
     toFirestore: (User u, _) =>  u.toFireStore()
   );
 
+  static final messageRef = FirebaseFirestore.instance
+  .collection("messages")
+  .withConverter(
+    fromFirestore: Message.fromFirestore,
+    toFirestore: (Message m, _) =>  m.toFireStore()
+  );
+
   static Future<void> addUser(User user) async {
-    await ref.doc(user.id).set(user);
+    await userRef.doc(user.id).set(user);
   }
 
   static Future<QuerySnapshot<User>> getUsersOnce() async {
-    return ref.get();
+    return userRef.get();
+  }
+
+  static Future<void> addMessage(Message message) async {
+    await messageRef.doc(message.messageID).set(message);
+  }
+
+  static Future<QuerySnapshot<Message>> getMessagesOnce() async {
+    return messageRef.get();
   }
  }
