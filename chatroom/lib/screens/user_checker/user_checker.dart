5bce5a1a8893f5de566f7593f69c94f196ef6015
nicole1drinkwater <nicnacsdrinkwater@gmail.com>
2025-07-30 19:18:10 +0200

    The user checker works as intended and the navigation doesn't allow the user to go back to screens they shouldn't be allowed to anymore

--- chatroom/lib/screens/user_checker/user_checker.dart
+++ chatroom/lib/screens/user_checker/user_checker.dart
@@ -1,4 +1,11 @@
+import 'package:chatroom/main.dart';
+import 'package:chatroom/screens/chat_room/chat_room.dart';
+import 'package:chatroom/screens/sign_up/sign_up.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../services/user_store.dart';
 
 class UserChecker extends StatefulWidget {
   const UserChecker({super.key});
@@ -11,18 +18,37 @@ class _UserCheckerState extends State<UserChecker> {
   @override
   void initState() {
     super.initState();
+
+     _checkUserSessionAndNavigate();
   }
 
-  Future<void> _checkUserSessionAndNavigate() {
+  Future<void> _checkUserSessionAndNavigate() async {
     final userStore = Provider.of<UserStore>(context, listen: false);
 
     final prefs = await SharedPreferences.getInstance();
 
     final userID = prefs.getString('userID');
+
+    if (userID != null) {
+      await userStore.loadUser(userID);
+
+      Navigator.pushReplacement(context, 
+        MaterialPageRoute(builder: (context) => const ChatRoomScreen()),
+      );
+    }
+    else {
+      Navigator.pushReplacement(context,
+       MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Flutter Demo Home Page'))
+       );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      )
+    );
   }
 }
